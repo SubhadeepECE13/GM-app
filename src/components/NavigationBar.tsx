78155dfc6ccf2fe857896643159db8b095f7864a
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-08-14 14:41:08 +0530

    refactor: restructure navbar components

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity, Image, View} from 'react-native';
+import Home_image from '../assets/Home_image.png';
+import play_image from '../assets/play_image.png';
+import notepad_image from '../assets/notepad_image.png';
+import notes_image from '../assets/notes_image.png';
+import account_image from '../assets/account_image.png';
+
+const NavigationBar = () => {
+  return (
+    <View style={{flexDirection: 'row', marginTop: 10, marginBottom: 10}}>
+      <TouchableOpacity style={styles.button}>
+        <Image source={Home_image} style={styles.image} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Image source={play_image} style={styles.image} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Image source={notepad_image} style={styles.image} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Image source={notes_image} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Image source={account_image} style={styles.image} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  button: {
+    width: '20%',
+    padding: 20,
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  image: {
+    height: 30,
+    width: 30,
+    borderRadius: 10,
+    marginRight: 20,
+  },
+});
+export default NavigationBar;
--- src/pages/Orthopage.tsx
+++ src/pages/Orthopage.tsx
@@ -1,21 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, ImageBackground, } from 'react-native'
+import Card from '../components/Card'
+import NavigationBar from '../components/NavigationBar'
 import ham_image from '../assets/ham_image.png'
 import Arrow_image from '../assets/Arrow_image.png'
 import image_1 from '../assets/image_1.png'
 import image_2 from '../assets/image_2.png'
-import Home_image from '../assets/Home_image.png'
-import play_image from '../assets/play_image.png'
-import notepad_image from '../assets/notepad_image.png'
-import notes_image from '../assets/notes_image.png'
-import account_image from '../assets/account_image.png'
-import React from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, ImageBackground, } from 'react-native'
-import Card from '../components/Card'
-import Navigation from '../components/Navigation'
 
 const Orthopage = () => {
     const { width, height } = Dimensions.get('window')
+    
     return (
-        <View style={{ display: 'flex', justifyContent: "space-between", alignItems: 'center', height, width }}>
+        <View style={[styles.conainer,{ height, width }]}>
             <View style={{ flexDirection: "row", justifyContent: "space-between", padding: 15, width }}>
                 <TouchableOpacity>
                     <Image source={Arrow_image} style={{ width: 20, height: 20, marginLeft: 5, marginTop: 5 }} />
@@ -28,8 +24,8 @@ const Orthopage = () => {
 
             </View>
             <View>
-                <Text style={[styles.welcomeText, { textAlign:"left" ,fontWeight:"900",marginRight:20}]}> GP and ORTHO</Text>
-                <Text style={[styles.headingText, {}]}> Lorem Ipsum</Text>
+                <Text style={styles.welcomeText}> GP and ORTHO</Text>
+                <Text style={styles.headingText}> Lorem Ipsum</Text>
                 <TouchableOpacity>
                     <Text style={[styles.text, {}]}> View All ></Text>
                 </TouchableOpacity>
@@ -37,40 +33,34 @@ const Orthopage = () => {
             </View>
 
 
-            <View style={{ display: 'flex', justifyContent: "space-between", alignItems: "center", marginBottom: 120, marginTop: 10, height: "50%", width, borderRadius: 90, }}>
+            <View style={[styles.cardsSection,{ width}]}>
                 <Card bgColor="#354169" imageUrl={image_1} heading="IMAGE" about="Lorem ipsum dolor sit amet," textColor="#FFFFFF" text="Lorem ipsum dolor sit amet," />
                 <Card bgColor="#C8A64E" imageUrl={image_2} heading="PDF" about="Lorem ipsum dolor sit amet," textColor="#000000" text="Lorem ipsum dolor sit amet," />
             </View>
-            <View style={{ flexDirection: 'row', marginTop: 10, marginBottom: 10 }}>
-
-                <Navigation imageUrl={Home_image} />
-                <Navigation imageUrl={play_image} />
-                <Navigation imageUrl={notepad_image} />
-                <Navigation imageUrl={notes_image} />
-                <Navigation imageUrl={account_image} />
-            </View>
+            <NavigationBar/>
         </View>
-
-
-
-
-
-
-
-
-
     )
 }
 
 const styles = StyleSheet.create({
+    conainer:{
+        display: 'flex',
+        justifyContent: "space-between",
+        alignItems: 'center'
+    },
     headingText: {
-
         fontWeight: 'bold',
         color: '#3B3939',
         textAlign: 'left',
-        right: "15%",
-        top: "17%"
-
+    },
+    cardsSection:{
+        display: 'flex',
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 120,
+        marginTop: 10,
+        height: "50%",
+        borderRadius: 90, 
     },
     text: {
         textAlign: "right",
@@ -81,31 +71,13 @@ const styles = StyleSheet.create({
         fontSize: 10,
 
     },
-    button: {
-
-        paddingVertical: 10,
-        borderRadius: 280,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        top: 560,
-        bottom: 90,
-        marginTop: 200,
-        height: 50,
-        left: '10%',
-
-    },
-    buttonText: {
-        color: 'Red',
-        fontSize: 35,
-        fontWeight: 'bold',
-    },
     welcomeText: {
-
         fontWeight: "bold",
         color: '#354169',
         fontSize: 35,
-
+        textAlign:"left",
+        fontWeight:"900",
+        marginRight:20
     },
 });
 
